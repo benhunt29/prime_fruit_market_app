07792be993bdb0626de2a333c3a9db87f833e865
Kim <kimhayden@Kims-MacBook-Air.local>
2015-08-18 11:10:44 -0500

    fruit constructor

---
+++ script.js
@@ -0,0 +1,14 @@
+function Fruit(type, cost){
+
+	this.type = type;
+	this.cost = function (cost){
+		if(cost > 9.99){
+			this.cost = 9.99;}
+		else if(cost < 0.5) {
+			this.cost = 0.5;
+		}
+		else {
+			this.cost = cost:
+		}
+
+};
