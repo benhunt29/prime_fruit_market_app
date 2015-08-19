1e5a58918d867651d811a5ae3a8b8a9a90c77187
Jason Spidle <jason.spidle@gmail.com>
2015-08-19 08:54:06 -0500

    Random price function

---
+++ randomCost.js
@@ -0,0 +1,20 @@
+function randomPrice (){
+	var randomDollars= Math.floor(Math.random()*10);
+	var randomCents= Math.floor(Math.random()*100);
+	var randomMonies= parseFloat(randomDollars + '.' + randomCents);
+	var result= Math.round((10-randomMonies)*100)/100;
+	
+	if (result < 0.5){
+		var cost = 0.50;
+	}
+	else if (result > 9.99){
+		var cost = 9.99
+	}
+	else{
+		var cost = result;
+	}
+
+	return cost;
+}
+
+console.log(cost);
